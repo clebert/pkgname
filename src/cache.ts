bdda47cfa118a95ff6870d0552febc948cd305ff
Clemens Akens <clebert@me.com>
2017-01-29 23:47:28 +0100

    feat: set config name to package version

--- src/cache.ts
+++ src/cache.ts
@@ -1,7 +1,10 @@
 import Conf = require('conf');
 
+// tslint:disable-next-line no-var-requires
+const configName = require('../package.json').version;
+
 let cache: Conf | undefined;
 
 export function getCache(): Conf {
-  return cache ? cache : (cache = new Conf({ configName: 'v2' }));
+  return cache ? cache : (cache = new Conf({ configName }));
 }
